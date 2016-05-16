c96f50b95b44b3380865c634775276d97acfa74d
Maxime <maxime_m@live.ca>
2016-05-16 10:22:13 -0500

    index resizitionné

--- index.php
+++ index.php
@@ -112,89 +112,90 @@ function getImageSource(piece)
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
         <title></title>
     </head>
-    <body onresize="setSquare()" onload="setSquare()">
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-        <div class="row" id="row">
-            <divCase1 class="col-xs-1 col-xs-offset-2" id="case00"></divCase1>
-            <divCase1 class="col-xs-1" id="case01"></divCase1>
-            <divCase1 class="col-xs-1" id="case02"></divCase1>
-            <divCase1 class="col-xs-1" id="case03"></divCase1>
-            <divCase1 class="col-xs-1" id="case04"></divCase1>
-            <divCase1 class="col-xs-1" id="case05"></divCase1>
-            <divCase1 class="col-xs-1" id="case06"></divCase1>
-            <divCase1 class="col-xs-1" id="case07"></divCase1>
+    <body onresize="setSquare()" onload="setSquare()" class="col-xs-offset-3 col-xs-8">
+        <div id="grille">
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+            <div class="row" id="row">
+                <divCase1 class="col-xs-1" id="case00"></divCase1>
+                <divCase1 class="col-xs-1" id="case01"></divCase1>
+                <divCase1 class="col-xs-1" id="case02"></divCase1>
+                <divCase1 class="col-xs-1" id="case03"></divCase1>
+                <divCase1 class="col-xs-1" id="case04"></divCase1>
+                <divCase1 class="col-xs-1" id="case05"></divCase1>
+                <divCase1 class="col-xs-1" id="case06"></divCase1>
+                <divCase1 class="col-xs-1" id="case07"></divCase1>
+            </div>
+            <div class="row">
+                <divCase2 class="col-xs-1" id="case10"></divCase2>
+                <divCase2 class="col-xs-1" id="case11"></divCase2>
+                <divCase2 class="col-xs-1" id="case12"></divCase2>
+                <divCase2 class="col-xs-1" id="case13"></divCase2>
+                <divCase2 class="col-xs-1" id="case14"></divCase2>
+                <divCase2 class="col-xs-1" id="case15"></divCase2>
+                <divCase2 class="col-xs-1" id="case16"></divCase2>
+                <divCase2 class="col-xs-1" id="case17"></divCase2>
+            </div>
+            <div class="row">
+                <divCase1 class="col-xs-1" id="case20"></divCase1>
+                <divCase1 class="col-xs-1" id="case21"></divCase1>
+                <divCase1 class="col-xs-1" id="case22"></divCase1>
+                <divCase1 class="col-xs-1" id="case23"></divCase1>
+                <divCase1 class="col-xs-1" id="case24"></divCase1>
+                <divCase1 class="col-xs-1" id="case25"></divCase1>
+                <divCase1 class="col-xs-1" id="case26"></divCase1>
+                <divCase1 class="col-xs-1" id="case27"></divCase1>
+            </div>
+             <div class="row">
+                <divCase2 class="col-xs-1" id="case30"></divCase2>
+                <divCase2 class="col-xs-1" id="case31"></divCase2>
+                <divCase2 class="col-xs-1" id="case32"></divCase2>
+                <divCase2 class="col-xs-1" id="case33"></divCase2>
+                <divCase2 class="col-xs-1" id="case34"></divCase2>
+                <divCase2 class="col-xs-1" id="case35"></divCase2>
+                <divCase2 class="col-xs-1" id="case36"></divCase2>
+                <divCase2 class="col-xs-1" id="case37"></divCase2>
+            </div>
+            <div class="row">
+                <divCase1 class="col-xs-1" id="case40"></divCase1>
+                <divCase1 class="col-xs-1" id="case41"></divCase1>
+                <divCase1 class="col-xs-1" id="case42"></divCase1>
+                <divCase1 class="col-xs-1" id="case43"></divCase1>
+                <divCase1 class="col-xs-1" id="case44"></divCase1>
+                <divCase1 class="col-xs-1" id="case45"></divCase1>
+                <divCase1 class="col-xs-1" id="case46"></divCase1>
+                <divCase1 class="col-xs-1" id="case47"></divCase1>
+            </div>
+            <div class="row">
+                <divCase2 class="col-xs-1" id="case50"></divCase2>
+                <divCase2 class="col-xs-1" id="case51"></divCase2>
+                <divCase2 class="col-xs-1" id="case52"></divCase2>
+                <divCase2 class="col-xs-1" id="case53"></divCase2>
+                <divCase2 class="col-xs-1" id="case54"></divCase2>
+                <divCase2 class="col-xs-1" id="case55"></divCase2>
+                <divCase2 class="col-xs-1" id="case56"></divCase2>
+                <divCase2 class="col-xs-1" id="case57"></divCase2>
+            </div>
+            <div class="row">
+                <divCase1 class="col-xs-1" id="60"></divCase1>
+                <divCase1 class="col-xs-1" id="case61"></divCase1>
+                <divCase1 class="col-xs-1" id="case62"></divCase1>
+                <divCase1 class="col-xs-1" id="case63"></divCase1>
+                <divCase1 class="col-xs-1" id="case64"></divCase1>
+                <divCase1 class="col-xs-1" id="case65"></divCase1>
+                <divCase1 class="col-xs-1" id="case66"></divCase1>
+                <divCase1 class="col-xs-1" id="case67"></divCase1>
+            </div>
+            <div class="row">
+                <divCase2 class="col-xs-1" id="case70"></divCase2>
+                <divCase2 class="col-xs-1" id="case71"></divCase2>
+                <divCase2 class="col-xs-1" id="case72"></divCase2>
+                <divCase2 class="col-xs-1" id="case73"></divCase2>
+                <divCase2 class="col-xs-1" id="case74"></divCase2>
+                <divCase2 class="col-xs-1" id="case75"></divCase2>
+                <divCase2 class="col-xs-1" id="case76"></divCase2>
+                <divCase2 class="col-xs-1" id="case77"></divCase2>
+            </div>
         </div>
-        <div class="row">
-            <divCase2 class="col-xs-1 col-xs-offset-2" id="case10"></divCase2>
-            <divCase2 class="col-xs-1" id="case11"></divCase2>
-            <divCase2 class="col-xs-1" id="case12"></divCase2>
-            <divCase2 class="col-xs-1" id="case13"></divCase2>
-            <divCase2 class="col-xs-1" id="case14"></divCase2>
-            <divCase2 class="col-xs-1" id="case15"></divCase2>
-            <divCase2 class="col-xs-1" id="case16"></divCase2>
-            <divCase2 class="col-xs-1" id="case17"></divCase2>
-        </div>
-        <div class="row">
-            <divCase1 class="col-xs-1 col-xs-offset-2" id="case20"></divCase1>
-            <divCase1 class="col-xs-1" id="case21"></divCase1>
-            <divCase1 class="col-xs-1" id="case22"></divCase1>
-            <divCase1 class="col-xs-1" id="case23"></divCase1>
-            <divCase1 class="col-xs-1" id="case24"></divCase1>
-            <divCase1 class="col-xs-1" id="case25"></divCase1>
-            <divCase1 class="col-xs-1" id="case26"></divCase1>
-            <divCase1 class="col-xs-1" id="case27"></divCase1>
-        </div>
-         <div class="row">
-            <divCase2 class="col-xs-1 col-xs-offset-2" id="case30"></divCase2>
-            <divCase2 class="col-xs-1" id="case31"></divCase2>
-            <divCase2 class="col-xs-1" id="case32"></divCase2>
-            <divCase2 class="col-xs-1" id="case33"></divCase2>
-            <divCase2 class="col-xs-1" id="case34"></divCase2>
-            <divCase2 class="col-xs-1" id="case35"></divCase2>
-            <divCase2 class="col-xs-1" id="case36"></divCase2>
-            <divCase2 class="col-xs-1" id="case37"></divCase2>
-        </div>
-        <div class="row">
-             <divCase1 class="col-xs-1 col-xs-offset-2" id="case40"></divCase1>
-            <divCase1 class="col-xs-1" id="case41"></divCase1>
-            <divCase1 class="col-xs-1" id="case42"></divCase1>
-            <divCase1 class="col-xs-1" id="case43"></divCase1>
-            <divCase1 class="col-xs-1" id="case44"></divCase1>
-            <divCase1 class="col-xs-1" id="case45"></divCase1>
-            <divCase1 class="col-xs-1" id="case46"></divCase1>
-            <divCase1 class="col-xs-1" id="case47"></divCase1>
-        </div>
-        <div class="row">
-            <divCase2 class="col-xs-1 col-xs-offset-2" id="case50"></divCase2>
-            <divCase2 class="col-xs-1" id="case51"></divCase2>
-            <divCase2 class="col-xs-1" id="case52"></divCase2>
-            <divCase2 class="col-xs-1" id="case53"></divCase2>
-            <divCase2 class="col-xs-1" id="case54"></divCase2>
-            <divCase2 class="col-xs-1" id="case55"></divCase2>
-            <divCase2 class="col-xs-1" id="case56"></divCase2>
-            <divCase2 class="col-xs-1" id="case57"></divCase2>
-        </div>
-        <div class="row">
-            <divCase1 class="col-xs-1 col-xs-offset-2" id="60"></divCase1>
-            <divCase1 class="col-xs-1" id="case61"></divCase1>
-            <divCase1 class="col-xs-1" id="case62"></divCase1>
-            <divCase1 class="col-xs-1" id="case63"></divCase1>
-            <divCase1 class="col-xs-1" id="case64"></divCase1>
-            <divCase1 class="col-xs-1" id="case65"></divCase1>
-            <divCase1 class="col-xs-1" id="case66"></divCase1>
-            <divCase1 class="col-xs-1" id="case67"></divCase1>
-        </div>
-        <div class="row">
-            <divCase2 class="col-xs-1 col-xs-offset-2" id="case70"></divCase2>
-            <divCase2 class="col-xs-1" id="case71"></divCase2>
-            <divCase2 class="col-xs-1" id="case72"></divCase2>
-            <divCase2 class="col-xs-1" id="case73"></divCase2>
-            <divCase2 class="col-xs-1" id="case74"></divCase2>
-            <divCase2 class="col-xs-1" id="case75"></divCase2>
-            <divCase2 class="col-xs-1" id="case76"></divCase2>
-            <divCase2 class="col-xs-1" id="case77"></divCase2>
-        </div>
-        
         <script>
             for(var i = 0; i < 64; i++)
             {
