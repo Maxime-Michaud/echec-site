506c2b207e03400ef774f155dc3a6d690b2600cf
potatokev <sorrynomana4u@gmail.com>
2016-05-16 09:20:46 -0400

    le frame des page

--- defi.php
+++ defi.php
@@ -21,8 +21,8 @@ and open the template in the editor.
 		<div class="row">
 			<div class="col-xs-12">
 				 <ul class="nav nav-pills nav-justified" role="tablist">
-					<li class="active"><a href="index.php">Acceuil</a></li>
-					<li><a href="defi.php">Les défis</a></li>
+					<li><a href="index.php">Acceuil</a></li>
+					<li  class="active"><a href="defi.php">Les défis</a></li>
 					<li><a href="client.php">Client</a></li>
 					<li><a href="statistique.php">Statistique</a></li>   
 					<li><a href="historique.php">Historique des parties</a></li>
@@ -30,19 +30,9 @@ and open the template in the editor.
 				  </ul>
 			</div>
 		</div>
-		<div class="row">
-			<div class="col-xs-12">
-				<p class="text-center">Le jeu NOMTEMPORAIREPATATE qui permet aux utilisateurs de s'améliorer aux échecs grace à :</p>
-				<ul class="ulnone">
-				   <li class="text-center"> Un système qui analyse vos parties pour ensuite faire des suggestions. </li>
-				   <li class="text-center"> Une liste de défi pour améliorer vos techniques de jeu. </li> 
-				   <li class="text-center"> La possibilité de jouer contre une intelligence artificiel ayant plusieurs niveau de difficulter </li> 
-				   <li class="text-center"> La possibilité de jouer des parties contres des amies </li> 
-				</ul>
-			</div>
-		</div>
-        <?php
+		<?php
 			echo "//TODO le look de la page plus lien";
         ?>
+      
     </body>
 </html>
